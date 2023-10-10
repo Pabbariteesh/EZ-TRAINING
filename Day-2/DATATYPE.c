df3c641ecf83644ee750458057e4eb54b04b6dbe
PABBA RITEESH <137896149+Pabbariteesh@users.noreply.github.com>
2023-10-10 11:28:29 +0530

    Add files via upload

---
+++ Day-2/DATATYPE.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	long long int mobile=8374806991;
+	printf("mobile number:%lld",mobile);
+	int pincode=506011;
+	printf("\nPincode:%i",pincode);
+	long long int Aadharnumber=4513086004260232;
+	printf("\nAadhar number:%lld",Aadharnumber);
+	short Age=128;
+	printf("\nAge=%hi",Age);
+	long long int Creditcardnumber=9999999999999999;
+	printf("\nCredit card number:%lld",Creditcardnumber);
+}
